578fba989a3bb6751bb976157c28f3c02e2ceb40
m3yevn <pyaehlianmoe@gmail.com>
2020-08-30 16:52:55 +0800

    update: select component

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,43 @@
 import React, { useState } from "react";
-import TextInput from "./components/text-input";
-import Card from "./components/card";
+//import TextInput from "./components/text-input";
+//import Card from "./components/card";
 import Select from "./components/select";
 
 function App() {
-  const [text, setText] = useState("97330256");
-  const [error, setError] = useState("");
-
-  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setError("");
+  const [text, setText] = useState("");
+  //const [error, setError] = useState("");
+  //
+  //const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //  setError("");
+  //  setText(e.target.value);
+  //};
+  //
+  //const handleOnError = (e: React.SyntheticEvent<HTMLInputElement, Event>) => {
+  //  setError(e.currentTarget.validationMessage);
+  //};
+  //
+  const handleOnChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setText(e.target.value);
-  };
-
-  const handleOnError = (e: React.SyntheticEvent<HTMLInputElement, Event>) => {
-    setError(e.currentTarget.validationMessage);
   };
 
   return (
     <div>
       <h1>Component Tests</h1>
-      <Select>
-        <option>1. Hello</option>
-        <option>2. World</option>
-        <option>3. Dev</option>
+      <Select onChange={handleOnChange} title="Select One">
+        <option value="">Select One</option>
+        <option value="1">1. Hello</option>
+        <option value="2">2. World</option>
+        <option value="3">3. Dev</option>
       </Select>
+      <div>{text}</div>
+      <hr/>
+      <Select native onChange={handleOnChange} title="Select One">
+        <option value="">Select One</option>
+        <option value="1">1. Hello</option>
+        <option value="2">2. World</option>
+        <option value="3">3. Dev</option>
+      </Select>
+      <div>{text}</div>
     </div>
   );
 }
--- src/components/select/Select.tsx
+++ src/components/select/Select.tsx
@@ -12,6 +12,14 @@ interface ISelected {
   value: any;
 }
 
+interface ISelect {
+  children: React.ReactNode;
+  title?: string;
+  defaultValue?: string;
+  native?: boolean;
+  onChange?: any;
+}
+
 const selectedState: ISelected = {
   title: "",
   value: "",
@@ -22,29 +30,108 @@ const selectedReducer = (state = selectedState, action: IAction) => {
   return reducedState;
 };
 
-export default function Select(
-  props: React.DetailedHTMLProps<
-    React.HTMLAttributes<HTMLSelectElement>,
-    HTMLSelectElement
-  >
-) {
-  const { children, title, defaultValue: value } = props;
+export default function Select(props: ISelect) {
+  const { children, title, defaultValue: value, native, onChange } = props;
+
   const [selected, setSelected] = useReducer(selectedReducer, {
     ...selectedState,
-    title,
-    value,
+    title: title || "",
+    value: value || "",
   } as ISelected);
 
-  const handleClick = () => {};
+  const [toggleShowOpts, setToggleShowOpts] = useState(false);
+  const [options, setOptions] = useState<ISelected[]>([]);
+  const selectRef = useRef<HTMLSelectElement>(null);
+
+  const handleClickSelect = () => {
+    setToggleShowOpts(!toggleShowOpts);
+  };
+
+  const handleClickLink = (selected: ISelected) => {
+    if (!!selectRef.current?.value) {
+      selectRef.current.value = selected.value;
+    }
+    if (!!onChange) {
+      const event = {
+        target: {
+          title: selected.title,
+          value: selected.value,
+        },
+      };
+      onChange(event);
+    }
+    setSelected({ type: "title", payload: selected.title });
+    setSelected({ type: "value", payload: selected.value });
+  };
+
+  useEffect(() => {
+    if (native) {
+      return;
+    }
+    const options = selectRef?.current?.options;
+
+    if (!!options) {
+      const optionArray: ISelected[] = [];
+      for (let i = 0; i < options?.length; i++) {
+        const option = {
+          title: options[i].text,
+          value: options[i].value,
+        };
+
+        optionArray.push(option);
+      }
+      setOptions(optionArray);
+    }
+  }, []);
 
   return (
-    <div className="rts-ui__select" onClick={handleClick}>
-      {selected.title}
-      <i className="rts-ui__select__arrow">
-        <Chevron />
-      </i>
-      <select>{children}</select>
-      <div className="rts-ui__select__option_panel"></div>
-    </div>
+    <>
+      {!!native ? (
+        <select className="rts-ui__select" {...props}>
+          {children}
+        </select>
+      ) : (
+        <>
+          <div className="rts-ui__select" onClick={handleClickSelect} onBlur={handleClickSelect}>
+            <div className="rts-ui__select__visible">
+              <label>{selected.title}</label>
+              <i className="rts-ui__select__arrow">
+                <Chevron />
+              </i>
+            </div>
+            <select
+              ref={selectRef}
+              className="rts-ui__select--hidden"
+              {...props}
+            >
+              {children}
+            </select>
+            {toggleShowOpts && (
+              <div className="rts-ui__select__option_panel">
+                <ul>
+                  {!!options?.length &&
+                    options?.map((option, index) => (
+                      <li
+                        className={
+                          option.value === selected.value
+                            ? "rts-ui__select__option--selected"
+                            : ""
+                        }
+                        key={index}
+                        id={index.toString()}
+                        onClick={() => {
+                          handleClickLink(option);
+                        }}
+                      >
+                        {option.title}
+                      </li>
+                    ))}
+                </ul>
+              </div>
+            )}
+          </div>
+        </>
+      )}
+    </>
   );
 }
