e003f1bc05c94e3d940c91995d6369d3c6175f39
m3yevn <pyaehlianmoe@gmail.com>
2020-09-13 12:59:55 +0800

    bump: translator to 1.0.4

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,7 @@
-export { default } from "./Select";
+/**
+ * @name Translator
+ * @consists {TranslateProvider}
+ * @consists {Translatable}
+ * @description TranslateProvider and Translatable JSX elements for localization
+ */
+export * from "./Translator";
--- src/components/translator/index.js
+++ src/components/translator/index.js
@@ -1,1 +1,7 @@
+/**
+ * @name Translator
+ * @consists {TranslateProvider}
+ * @consists {Translatable}
+ * @description TranslateProvider and Translatable JSX elements for localization
+ */
 export * from "./Translator";
